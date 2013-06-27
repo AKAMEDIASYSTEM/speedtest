4dd00e4b2a1a2c630f5aba859547910269faa26c
AKAMEDIASYSTEM <nfeehan@gmail.com>
2013-06-26 22:09:51 -0400

    beaglePWM servo attempts

--- beaglePWM.py
+++ beaglePWM.py
@@ -34,7 +34,7 @@ for j in range(100):
 	rot = mapVals(j,0.0,100.0,5.0,10.0)
 	print rot
 	pwm.start(greenPin, 50, 2000)
-	pwm.start(bluePin, rot, 50) # 50Hz
+	pwm.start(bluePin, rot, 50.0) # 50Hz
 	pwm.start(redPin, 0, 2000)
 
 	# rot = mapVals(j,0.0,100.0,5.0,20.0)
