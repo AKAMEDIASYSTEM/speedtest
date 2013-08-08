743b9ae12ca562b97979fa0086411f5e8c7e8106
Noah Feehan <nfeehan@gmail.com>
2013-08-08 12:09:35 -0400

    added exit handling to prevent PWM mess

--- PWM_demo.py
+++ PWM_demo.py
@@ -1,10 +1,21 @@
 import time
 import Adafruit_BBIO.PWM as pwm
+import atexit
+
 redPin = 'P8_19'
 greenPin = 'P8_13'
 interval = 0.25
 pwm.start(redPin,50,2000)
 pwm.start(greenPin,50,2000)
+
+def exit_handler():
+    print 'exiting'
+    pwm.stop(greenPin)
+    pwm.stop(redPin)
+    pwm.cleanup()
+
+atexit.register(exit_handler)
+
 while True:
 	for i in range(0,100):
 		pwm.set_duty_cycle(redPin,float(i))
