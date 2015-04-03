2c9a3e18e21e11870025c07c481712c570aabb75
akamediasystem <nfeehan@gmail.com>
2015-04-03 11:51:53 -0400

    adding blinker stub

--- blinker.py
+++ blinker.py
@@ -48,6 +48,7 @@ def do_blink():
     print current
     print current['DL']
     print ulAv
+    print min(ulAv)
     ulOutput = mapVals(current['UL'], min(ulAv),max(ulAv),0,255)
     print ulOutput
     dlOutput = mapVals(current['DL'], min(dlAv),max(dlAv),0,255)
