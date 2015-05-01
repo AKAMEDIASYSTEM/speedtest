4923554be0ddf26d2df6efa9c94c9b00dd65a35b
akamediasystem <nfeehan@gmail.com>
2015-05-01 15:02:07 -0400

    cleanup and adding webhandler functionality

--- handlers/WebHandler.py
+++ handlers/WebHandler.py
@@ -53,7 +53,8 @@ class WebHandler(BaseHandler):
         dlOutput = self.mapVals(float(current['DL']), min(dlAv), max(dlAv), 0, 255)
         pingOutput = 10.0*self.mapVals(float(current['ping']), min(pingAv), max(pingAv), 0, 255)  # times 10 just for pingtime to be noticeable
         print ulOutput, dlOutput, pingOutput
-        keywords = []
-        print keywords
-        self.write(loader.load("speedtest.html").generate(keywords=keywords))
+        tcolor = [ulOutput,dlOutput]
+        keywords = [current]
+        print 'keywords is ', keywords
+        self.write(loader.load("speedtest.html").generate(keywords=keywords, color=tcolor))
         self.finish()
