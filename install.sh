eefe2ab9c66870b0fe12e3b93a9a6de52468714c
root <root@cafe.(none)>
2015-09-27 20:25:20 -0400

    updates

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 echo 'installing speedtest-barnacle now'
-chmod +x expire.sh
-cp speedtest-poller.service speedtest-avahi.service speedtest-resonator.service speedtest-blinker.service speedtest-server.service /etc/systemd/system
+cp speedtest-poller.service speedtest-resonator.service speedtest-blinker.service speedtest-server.service /etc/systemd/system
 cp avahi-resonator.service /etc/avahi/services/
-systemctl enable speedtest-poller.service speedtest-avahi.service speedtest-resonator.service speedtest-blinker.service speedtest-server.service
-systemctl start speedtest-poller.service speedtest-avahi.service speedtest-resonator.service speedtest-blinker.service speedtest-server.service
+systemctl enable speedtest-poller.service speedtest-resonator.service speedtest-blinker.service speedtest-server.service
+systemctl start speedtest-poller.service speedtest-resonator.service speedtest-blinker.service speedtest-server.service
 systemctl | grep AKA
